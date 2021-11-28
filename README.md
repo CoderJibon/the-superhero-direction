7c8794c2a008d24d387fa1b54e1dc58b0e4611cf
coderjibon <contact.engjibon@gmail.com>
2021-11-29 00:23:01 +0600

    the-superhero-direction

---
+++ README.md
@@ -0,0 +1,11 @@
+# Computer Programmers Team
+
+Computer Programmers Team Link: https://blissful-heyrovsky-e6c2ac.netlify.app/.
+
+. Top Programmer people
+
+. The person who writes computer programs.
+
+. Computer programmers write and test code that allows computer applications and software programs to function properly.
+
+. the man some make the programing language.
---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Programmers from './components/Programmers/Programmers';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-Container">
+        <Header></Header>
+        <Programmers></Programmers>
+      </div>
+    </div>
+  );
+}
+
+export default App;
